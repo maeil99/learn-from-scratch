2a0735fc1da10de03a4bab53069318073a1f4c30
maeil99 <ismailmohdnorit@gmail.com>
2021-08-03 13:33:12 +0800

    completed detail page and 404 page

--- src/components/BlogDetails.js
+++ src/components/BlogDetails.js
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 import useFetch from "../const/useFetch";
 
 const BlogDetails = () => {
@@ -10,6 +10,17 @@ const BlogDetails = () => {
     isLoading,
   } = useFetch("http://localhost:8000/blogs/" + id);
 
+  const history = useHistory();
+
+  //function to delete blog
+ const handleClick = () => {
+    fetch("http://localhost:8000/blogs/" + blog.id, {
+      method: 'DELETE'
+    }).then (()=> {
+      history.push('/');
+    })
+ }
+
   return (
     <div className="blog-details">
       {isLoading && <>Loading...</>}
@@ -19,6 +30,7 @@ const BlogDetails = () => {
           <h2>{blog.title}</h2>
           <p>Written by {blog.author}</p>
           <div>{blog.body}</div>
+          <button onClick={handleClick}>Delete Blog</button>
         </article>
       )}
     </div>
--- src/components/Create.js
+++ src/components/Create.js
@@ -2,17 +2,20 @@ import { useState } from "react";
 import { useHistory } from "react-router-dom";
 
 const Create = () => {
+  //set default value/state for each var and make it can set the current state
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
   const [author, setAuthor] = useState("mario");
   const [isLoading, setIsLoading] = useState(false);
   const history = useHistory();
 
+  //function after click add blog
   const handleSubmit = (e) => {
     e.preventDefault();
     setIsLoading(true);
     const blog = { title, body, author };
 
+    //make POST request
     fetch("http://localhost:8000/blogs/", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
